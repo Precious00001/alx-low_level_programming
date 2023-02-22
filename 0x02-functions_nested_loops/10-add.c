2c0d528e80cfe31dd02aa883893c6d13f78cf52d
Precious00001 <lanremidelola@gmail.com>
2023-02-21 20:12:20 -0800

    alx

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @a: para1
+ * @b: para2
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include "holberton.h"
+/**
+ *times_table - prints the 9 times table, starting with 0
+ *
+ */
+
+
+void times_table(void)
+{
+	int i, j, res;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			res = i * j;
+			if (j == 0)
+				_putchar('0');
+			else
+				print_number(res);
+
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
